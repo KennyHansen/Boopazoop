7ff6bea5d32874eb6647bbc63a70b5da916e3565
KennyHansen <hansen.kenny101@gmail.com>
2017-01-27 10:09:12 -0700

    Add reset button, improved console

--- game-controller.js
+++ game-controller.js
@@ -27,11 +27,11 @@ var GameController = function () {
         event.preventDefault()
         var roboNames = document.getElementById('robo-name-' + botNum)
         var text = roboNames.elements[0].value;
-        console.log(text)
         // document.getElementById("roboName").innerHTML = text;
         if (text) {
             game.generateBot(text, botNum)
             checkGameState()
+            toggleHealthDisplay(botNum)
         }
     }
 
@@ -49,7 +49,29 @@ var GameController = function () {
     }
 
     this.reset = function() {
+        if(checkGameState())  {
+            var roboName1 = document.getElementById('robo-name-1')
+            var roboName2 = document.getElementById('robo-name-2')
 
+            roboName1.innerHTML = `
+                <input type="text" class="player-name" placeholder="Enter robot name" maxLength="17" required></input>
+				<button type="submit">Build</button>
+            `
+
+            roboName2.innerHTML = `
+                <input type="text" class="player-name" placeholder="Enter robot name" maxLength="17" required></input>
+				<button type="submit">Build</button>
+            `
+
+            document.getElementById('robo-img-1').src = ''
+            document.getElementById('robo-img-2').src = ''
+
+            game.toggleResetButton()
+            game.updateLog("Game is Reset")
+            toggleFightButton()
+            toggleHealthDisplay(1)
+            toggleHealthDisplay(2)
+        }
     }
 
     function checkGameState() {
@@ -72,6 +94,17 @@ var GameController = function () {
         }
     }
 
+     function toggleHealthDisplay(botNum) {
+        var playerHealthId = document.getElementById('robo-health-' + botNum)
+        if (playerHealthId.style.display == 'block') {
+            playerHealthId.style.display = 'none';
+        } else {
+            playerHealthId.style.display = 'block';
+            var playerHealthTag = playerHealthId.getElementsByTagName("span");
+            playerHealthTag[0].style = "width:100%"
+        }
+    }
+
     function draw() {
         var health1 = game.getPlayerHealth('robo1')
         var health2 = game.getPlayerHealth('robo2')
@@ -84,7 +117,7 @@ var GameController = function () {
         var playerHealthTag = playerHealthId.getElementsByTagName("span");
         playerHealthTag[0].style = "width:" + healthPercent1 + "%"
 
-        var playerHealthId = document.getElementById("robo-health-1");
+        var playerHealthId = document.getElementById("robo-health-2");
         var playerHealthTag = playerHealthId.getElementsByTagName("span");
         playerHealthTag[0].style = "width:" + healthPercent2 + "%"
     }
